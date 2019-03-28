0ee0976f2789024022178ae6196e166f9c7b3a5e
lex <yobdc.101@gmail.com>
2019-03-28 17:05:29 +0800

    update

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
 var Crawler = require("crawler");
 
 var c = new Crawler({
-    maxConnections : 10,
+    maxConnections: 10,
     // This will be called for each crawled page
-    callback : function (error, res, done) {
-        if(error){
+    callback: function(error, res, done) {
+        if (error) {
             console.log(error);
-        }else{
+        } else {
             var $ = res.$;
             // $ is Cheerio by default
             //a lean implementation of core jQuery designed specifically for the server
-            console.log($(".sellListContent a").attr('href'));
+            console.log($(".sellListContent a").attr("href"));
         }
         done();
     }
 });
 
-var rootUrl = 'https://sh.lianjia.com';
+var rootUrl = "https://sh.lianjia.com";
 
-var handleDistrictPage = function(error, res, done){
+var handleDistrictPage = function(error, res, done) {
     var $ = res.$;
-    var districtList = $('.position div[data-role=ershoufang] div:first-child').children();
-    for(let i=0;i<districtList.length;i++) {
-        districtUrl = rootUrl + districtList[i].attribs['href'];
+    var districtList = $(
+        ".position div[data-role=ershoufang] div:first-child"
+    ).children();
+    for (let i = 0; i < districtList.length; i++) {
+        districtUrl = rootUrl + districtList[i].attribs["href"];
         c.queue([{
             uri: districtUrl,
             callback: handleVillagePage
         }]);
     }
     done();
-}
+};
 
-var handleVillagePage = function(error, res, done){
+var handleVillagePage = function(error, res, done) {
     var $ = res.$;
-    var districtList = $('.position div[data-role=ershoufang] div:last-child').children();
+    var districtList = $(
+        ".position div[data-role=ershoufang] div:last-child"
+    ).children();
     done();
 };
 
-
-var handleHousePage = function(error, res, done){
+var handleHousePage = function(error, res, done) {
+    var $ = res.$;
+    done();
 };
 
 c.queue([{
-    uri: rootUrl+'/ershoufang/',
+    uri: rootUrl + "/ershoufang/",
     callback: handleDistrictPage
 }]);
